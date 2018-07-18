c3c36aa37139e8b0ce073f0b886a65cf02a7eb43
Sam Thompson <noreply@gmail.com>
2018-07-18 18:20:37 +0200

    Add sidebar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './index.css'
 import MapView from './MapView'
+import Sidebar from './Sidebar'
 
 class App extends Component {
 
   render() {
     return (
       <div id="app">
+        <Sidebar />
         <MapView />
       </div>
     )
---
+++ src/Sidebar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './index.css'
+
+class Sidebar extends Component {
+
+  render() {
+    return (
+      <div className='sidebar'>
+        <h1>I will be the sidebar</h1>
+      </div>
+    )
+  }
+
+}
+
+export default Sidebar
