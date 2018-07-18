174dcd5d584576bc76e8792825b5a1384c15e3f5
Sam Thompson <noreply@gmail.com>
2018-07-18 19:14:20 +0200

    Add 5 places to search and API request for 1s data

--- src/App.js
+++ src/App.js
@@ -5,6 +5,32 @@ import Sidebar from './Sidebar'
 
 class App extends Component {
 
+  state = {
+    searchPlaces: [
+        {title: 'The Tower of London', search: 'tower+of+london'},
+        {title: 'Big Ben', search: 'big+ben,+london'},
+        {title: 'Buckingham Palace', search: 'buckingham+palace,+london'},
+        {title: 'The Shard', search: 'the+shard,+london'},
+        {title: 'The British Museum', search: 'the+british+museum,+london'}
+    ],
+    placesData: []
+  }
+
+  componentDidMount() {
+      this.getAllData()
+  }
+
+  getAllData = () => {
+      this.getData(this.state.searchPlaces[0])
+  }
+
+  getData = async (entry) => {
+      //API request here and map searchPlaces into placesData
+      const api_call = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${entry.search}&key=AIzaSyAQ8K05Bp11d0n6XLbb3eZd5vohjzGqWdU`)
+      const data = await api_call.json()
+      console.log(data)
+  }
+
   render() {
     return (
       <div id="app">
