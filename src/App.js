d9ed7d0b108d49ad89f66d750637cf77eef7899c
Sam Thompson <noreply@gmail.com>
2018-07-20 17:01:25 +0200

    filter working but then click doesnt match up

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ class App extends Component {
       this.tempData = this.state.places
       for (let i = 0; i < this.tempData.length; i++) {
           this.tempData[i].display = (this.tempData[i].title.toLowerCase().includes(query)) ? true : false
-          console.log(this.tempData[i].display)
       }
       this.setState({places:this.tempData})
   }
--- src/MapView.js
+++ src/MapView.js
@@ -5,49 +5,58 @@ class MapView extends React.Component {
     //Acts like the google maps callback init function
 
   markers = []
+
   // This has received new places props from parent so...WillReceiveProps lifecycle event
   componentWillReceiveProps(nextProps) {
 
       // Define self to allow it to be used within 'addEventListener'
       const self = this
+      for (let i = 0; i < this.markers.length; i++) {
+          this.markers[i].setMap(null)
+      }
+      this.markers = []
 
       //this.infoWindow = new window.google.maps.InfoWindow();
 
       // The following group uses the location array to create an array of markers on initialize.
 
-
-
       for (let i = 0; i < nextProps.places.length; i++) {
-        // Get the position from the location array.
-        let position = nextProps.places[i].latlng
-      //console.log(this.props.places[i])
-        //console.log(this.props.places[i].latlng)
-        let title = nextProps.places[i].title
-        //console.log(this.props.places[i].title)
-        // Create a marker per location, and put into markers array.
-        let marker = new window.google.maps.Marker({
-          map: this.map,
-          position: position,
-          title: title,
-          //animation: window.google.maps.Animation.DROP,
-          id: i
-        })
-        // Push the marker to our array of markers.
-        this.markers.push(marker);
+
+        if (nextProps.places[i].display === true) {
+            // Get the position from the location array.
+            let position = nextProps.places[i].latlng
+            //console.log(this.props.places[i])
+            //console.log(this.props.places[i].latlng)
+            let title = nextProps.places[i].title
+            //console.log(this.props.places[i].title)
+            // Create a marker per location, and put into markers array.
+            let marker = new window.google.maps.Marker({
+              map: this.map,
+              position: position,
+              title: title,
+              //animation: window.google.maps.Animation.DROP,
+              id: i
+            })
+            // Push the marker to our array of markers.
+            this.markers.push(marker);
 
 
-        // Create an onclick event to open an infowindow at each marker.
-        marker.addListener('click', function() {
-            self.activateMarker(this)
-        })
-        this.bounds.extend(this.markers[i].position)
+            // Create an onclick event to open an infowindow at each marker.
+            marker.addListener('click', function() {
+                self.activateMarker(this)
+            })
+            this.bounds.extend(nextProps.places[i].latlng)
+        }
+
     }
+
+    console.log(this.markers)
 
       //Extend the boundaries of the map for each marker
     this.map.fitBounds(this.bounds)
 
     // TODO When active marker id changes in parent state it is passed as new prop. Then populate the infowindow
-      this.openInfoWindow(nextProps.activeMarker)
+    this.openInfoWindow(nextProps.activeMarker)
   }
 
   activateMarker = (marker) => {
@@ -85,6 +94,11 @@ class MapView extends React.Component {
       zoom: 12,
       mapTypeId: 'roadmap',
     })
+    // Clear all markers
+    //for (let i = 0; i < this.markers.length; i++) {
+    //    this.markers[i].setMap(null)
+    //}
+    //this.markers = []
 
     this.infowindow = new window.google.maps.InfoWindow()
     this.bounds = new window.google.maps.LatLngBounds()
