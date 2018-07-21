1d7523d8d08a3fb8da29916301c0b7adb14fb126
Sam Thompson <noreply@gmail.com>
2018-07-21 14:25:31 +0200

    Fix tempWikiData now successfully obtains data required

--- src/App.js
+++ src/App.js
@@ -17,16 +17,17 @@ class App extends Component {
         {title: 'The Shard', search: 'the+shard,+london'},
         {title: 'The British Museum', search: 'the+british+museum,+london'}
     ],
-    wikiData: {},
     activeMarker: null,
     query: ''
   }
 
   tempData = []
   //Below is object with seperate index array so it can be correctly merged after both API async calls are made, as the order of reuturns is unknown
-  tempWikiData = {}
-  tempWikiDataIndex = []
+  tempWikiData = []
   search = 'https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&prop=pageimages|description&pageids='
+  wikiComplete = false
+  googleComplete = false
+  wikiDataCount = 0
 
   updateQuery = (query) => {
       this.setState({
@@ -54,14 +55,14 @@ class App extends Component {
 
   getAllData = () => {
       console.log('async getData call for each place')
+      this.tempData = []
+      //TODO If problems matching up later it could be this bit
+      this.tempWikiData = this.state.places
       for (let i = 0; i < this.state.places.length; i++) {
           //Google API requests
-          this.tempData = []
           this.getData(this.state.places[i])
           // Wiki API requests
-          this.tempWikiDataIndex = []
-          this.tempWikiData = {}
-          this.getWikiData(this.state.places[i])
+          this.getWikiData(this.tempWikiData[i])
       }
   }
   //Google API requests
@@ -80,8 +81,12 @@ class App extends Component {
               for (let i =0; i < this.tempData.length; i++) {
                   this.tempData[i].id = i
               }
-              // TODO Dont allow to setState until wiki also ready then do together
-              this.setState({places:this.tempData})
+              this.googleComplete = true
+              // Dont allow to setState until wiki also ready then do together
+              if (this.wikiComplete === true) {
+                  console.log('wiki api finished first')
+                  this.mergeApiData()
+              }
           }
       } else {
           alert(`There was an error with the Google API ${data.status}`)
@@ -94,17 +99,15 @@ class App extends Component {
       //API request here to create object searchable by title...as may be in differnt order to 'places'
       try {
           const api_call = await fetch(`https://en.wikipedia.org/w/api.php?action=query&origin=*&list=search&srlimit=1&format=json&srsearch=${entry.title}`)
-          console.log(api_call)
           const data = await api_call.json()
-          console.log(data)
           let item = entry
           item.pageid = data.query.search[0].pageid
-          this.tempWikiData[entry.title] = item
-          this.tempWikiDataIndex.push(data.query.search[0].pageid)
-
+          entry = item
+          this.wikiDataCount += 1
+          console.log(this.tempWikiData)
           //once all item pageids found run full data pull
-          if (this.tempWikiDataIndex.length === this.state.places.length) {
-              console.log(this.tempWikiDataIndex)
+          if (this.wikiDataCount === this.state.places.length) {
+              this.wikiDataCount = 0
               this.getAdditionalWikiData()
           }
 
@@ -116,10 +119,11 @@ class App extends Component {
 
   getAdditionalWikiData = async () => {
       //Make search string
-      let search = 'https://en.wikipedia.org/w/api.php?action=query&origin=*&prop=pageimages|description&format=json&formatversion=2&pageids=3969'
-      for (let i = 0; i < this.tempWikiDataIndex.length; i++) {
-          search = search + this.tempWikiDataIndex[i]
-          if (i < (this.tempWikiDataIndex.length - 1)) {
+      let search = 'https://en.wikipedia.org/w/api.php?action=query&origin=*&prop=pageimages|description&format=json&formatversion=2&pageids='
+      console.log(this.tempWikiData)
+      for (let i = 0; i < this.tempWikiData.length; i++) {
+          search = search + this.tempWikiData[i].pageid
+          if (i < (this.tempWikiData.length - 1)) {
               search = search + `|`
           }
       }
@@ -129,12 +133,54 @@ class App extends Component {
           const api_call = await fetch(`${search}`, {
           })
           let data = await api_call.json()
+
+          // Loop through all items in the tempWikiData which is indexed by PLACE title (not wikipedia page titles which may be similar/same), to add remaining data.
+          //TODO make this update the tempWikiData object correctly
           console.log(data)
+          console.log(this.tempWikiData)
+          for (let i = 0; i < this.tempWikiData.length; i++) {
+              let item = this.tempWikiData[i]
+              let num = item.pageid
+              console.log(num)
+              for (let i = 0; i < data.query.pages.length; i++) {
+                  let page = data.query.pages[i]
+                  console.log(page)
+                  console.log(page.pageid)
+                  if (page.pageid === num) {
+                      item.urlTitle = page.title
+                      console.log(page.title)
+                      item.description = page.description
+                      item.urlImage = page.pageimage
+                  }
+              }
+
+          }
+          console.log(this.tempWikiData)
+
+
+
+          // Dont allow to setState until wiki also ready then do together
+          this.wikiComplete = true
+          if (this.googleComplete === true) {
+              console.log('google api finished first')
+              this.mergeApiData()
+          }
+
 
 
 
       } catch (error) {console.log(error)}
       //TODO add better error handling
+  }
+
+  mergeApiData = () => {
+      //TODO will be called when both this.tempData array and this.tempWikiData objects are ready for merging and seting state.
+      this.wikiComplete = false
+      this.googleComplete = false
+//TODO change below
+      //console.log(data)
+      //console.log(this.tempWikiData)
+
   }
 
   render() {
