12df5b95e4957f14e52e896d30644bc3bde8b35b
Sam Thompson <noreply@gmail.com>
2018-07-23 09:58:08 +0200

    Add clear filter button

--- src/Filter.js
+++ src/Filter.js
@@ -7,10 +7,17 @@ class Filter extends Component {
       this.props.updateQuery(e.target.value.toLowerCase())
   }
 
+  resetFilter = () => {
+      this.props.updateQuery('')
+  }
+
   render() {
     return (
         <div>
-          <input type="text" placeholder="Filter List" value={this.props.query} onChange={this.filter}/>
+          <form>
+            <input type="text" placeholder="Filter List" value={this.props.query} onChange={this.filter}/>
+            <button type="button" onClick={this.resetFilter}>Clear Filter</button>
+          </form>
         </div>
     )
   }
